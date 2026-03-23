1fe6727d38f2c5681f9917151972f3d7070861a9
Kenenisa Beyan <kenenisab05@gmail.com>
2026-03-23 12:31:48 +0200

    implement User and Task models with password hashing

---
+++ backend/models/Task.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const taskSchema = new mongoose.Schema({
+  title: String,
+  description: String,
+  difficulty: String,
+  deadline: Date,
+  createdBy: { type: mongoose.Schema.Types.ObjectId, ref:'User' }
+});
+
+module.exports = mongoose.model('Task', taskSchema);
